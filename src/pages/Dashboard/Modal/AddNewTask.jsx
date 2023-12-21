2c05986fbf2b3bd0ce30f5f0b59175e479f82c1f
ashiqee <ashiq.buet73@gmail.com>
2023-12-21 21:22:36 +0600

    add Task form create

--- src/pages/Dashboard/Modal/AddNewTask.jsx
+++ src/pages/Dashboard/Modal/AddNewTask.jsx
@@ -1,10 +1,23 @@
 import { useState } from "react";
+import { useForm } from "react-hook-form";
+import Button from './../../../Components/Shared/Button/Button';
 
 
 const AddNewTask = ({ text }) => {
 
     const [isOpenModal, setOpenModal] = useState(false)
     const [taskPriotity, setTaskPriority] = useState('Low')
+    const { register, handleSubmit } = useForm()
+
+
+
+    const onSubmit = (data) => {
+
+        console.log(data);
+
+
+
+    }
 
     return (
 
@@ -36,28 +49,39 @@ const AddNewTask = ({ text }) => {
                                     </button>
                                 </div>
 
-                                <form className="p-4 md:p-5">
+                                <form onSubmit={handleSubmit(onSubmit)} className="p-4 md:p-5">
                                     <div className="grid gap-4 mb-4 grid-cols-2">
                                         <div className="col-span-2">
-                                            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Title</label>
-                                            <input type="text" name="title" id="title" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Type product name" required="" />
+                                            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Task Title</label>
+                                            <input type="text" {...register("title")} id="title" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Type task title" required="" />
                                         </div>
 
                                         <div className="col-span-2">
-                                            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Product Description</label>
-                                            <textarea id="description" rows="4" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write product description here"></textarea>
+                                            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Task Description</label>
+                                            <textarea {...register("description")} id="description" rows="4" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write task description here"></textarea>
                                         </div>
+                                        {/* <div className="col-span-2">
+                                            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Sub Task</label>
+                                            <div>
+                                                <input type="text" {...register("subTask")} id="subTask" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Type product name" required="" />
+                                            </div>
+                                        </div> */}
                                         <div className="col-span-2 sm:col-span-1">
                                             <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Task DeadLine</label>
-                                            <input type="date" name="taskDeadLine" id="taskDeadLine" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="$2999" required="" />
+                                            <input type="date" {...register("taskDeadLine")} id="taskDeadLine" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="$2999" required="" />
                                         </div>
                                         <div className="col-span-2 sm:col-span-1">
                                             <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Task Priority</label>
 
-                                            <select id="taskPriotity" value={taskPriotity} onChange={(e) => setTaskPriority(e.target.value)}>
-                                                <option value="Low">Low</option>
-                                                <option value="Moderate">Moderate</option>
-                                                <option value="High">High</option>
+                                            <select
+                                                {...register("taskPriority")} name="taskPriority" id="taskPriority"
+                                                className="select bg-inherit  select-info w-full max-w-xs"
+                                                defaultValue="Low"
+                                            >
+                                                <option disabled >Select Priority</option>
+                                                <option className="p-2 text-gray-800" value="Low">Low</option>
+                                                <option className="p-1 text-gray-800" value="Moderate">Moderate</option>
+                                                <option className="p-1 text-gray-800" value="High">High</option>
                                             </select>
                                         </div>
 
@@ -65,7 +89,7 @@ const AddNewTask = ({ text }) => {
                                     <div className="flex gap-5">
                                         <button type="submit" className="text-white btn border-none inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                                             <svg className="me-1 -ms-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" ></path></svg>
-                                            Add new product
+                                            Add Task
                                         </button>
 
                                     </div>
